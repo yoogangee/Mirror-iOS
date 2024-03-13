fb10f49509a56d3770de04bbe9937e6ac339b5d2
imdanaa <jangs1920@gmail.com>
2024-03-14 00:16:56 +0900

    MIRROR-27 [feat] 새로운 텍스트 배치시 해당 텍스트 읽어주기 기능 추가

--- Mirror_ios/Mirror_ios/Global/Util/PaddingLabel.swift
+++ Mirror_ios/Mirror_ios/Global/Util/PaddingLabel.swift
@@ -86,10 +86,10 @@ class ScrollPaddingLabel: UIScrollView {
         return label.text
     }
     
-    override var intrinsicContentSize: CGSize {
-        var contentSize = super.intrinsicContentSize
-        contentSize.height += padding.top + padding.bottom
-        contentSize.width += padding.left + padding.right
-        return contentSize
-    }
+//    override var intrinsicContentSize: CGSize {
+//        var contentSize = super.intrinsicContentSize
+//        contentSize.height += padding.top + padding.bottom
+//        contentSize.width += padding.left + padding.right
+//        return contentSize
+//    }
 }
